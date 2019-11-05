5621d282bffbe8aab56c58eacce874c1cd50f443
ahrke <david.duy.minh@gmail.com>
2019-11-05 16:00:28 -0500

    add Content and Result containers

---
+++ src/components/content.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+interface ContentInterface {
+  activeImage: string;
+  contentContainerRef: React.RefObject<any>;
+  textBottom: string;
+  textTop: string;
+}
+
+const Content = (props: ContentInterface) => {
+  return (
+    <div className="content" ref={props.contentContainerRef}>
+      <img src={props.activeImage} alt="Meme" />
+
+      <h1>{props.textTop}</h1>
+
+      <h2>{props.textBottom}</h2>
+    </div>
+  )
+}
+
+export default Content;
